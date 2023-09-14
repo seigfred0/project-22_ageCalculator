9c6ae5a315990fab86dce9e666b25f125f9a514d
seigfred0 <saysonseigfred@gmail.com>
2023-09-14 21:25:46 +0800

    finish the validation

--- app.js
+++ app.js
@@ -20,33 +20,54 @@ const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 
 
+
 function validate() {
     const inputs = document.querySelectorAll('.input');
-    let validate = true;
+    const inputColor = document.querySelectorAll('.input-day-text');
+    let isValid = true;
 
-    inputs.forEach((x) => {
+    inputs.forEach((x, index) => {
         const parent = x.parentElement;
 
         if (!x.value) {
             x.style.borderColor = 'red';
             parent.querySelector("small").innerText = 'This field is required';
-            validate = false;
-        } else if (monthInput.value > 12) {
+            inputColor[index].style.color = "red";
+            isValid = false;
+        } else if (x === monthInput && (x.value < 1 || x.value > 12)) {
             monthInput.style.borderColor = "red";
             monthInput.parentElement.querySelector("small").innerText = 'must be valid month';
-            validate = false;
-        } else if (dayInput.valid > 31) {
+            inputColor[index].style.color = "red";
+            isValid = false;
+        } else if (x === dayInput && (x.value < 1 || x.value > 31)) {
             dayInput.style.borderColor = "red";
-            dayInput.parentElement.querySelector("small") = "day incorrect";
-            validate = false;
+            dayInput.parentElement.querySelector("small").innerText = "must be valid Day";
+            inputColor[index].style.color = "red";
+            isValid = false;
+        } else if (x === yearInput && (x.value < 1950 || x.value > 2023)) {
+             yearInput.style.borderColor = "red";
+             yearInput.parentElement.querySelector("small").innerText = "Incorrect Year";
+             inputColor[index].style.color = "red";
+             isValid = false;
         } else {
             x.style.borderColor = "black";
             parent.querySelector("small").innerText = "";
-            validate = true;
+            inputColor[index].style.color = 'black';
+            isValid = true
+
         }
     });
 
-    return validate;
+    return isValid;
 };
 
-button.addEventListener('click', validate);
+
+function final() {
+    if (validate()) {
+        console.log("passed");
+    }
+}
+
+button.addEventListener('click', final);
+
+
